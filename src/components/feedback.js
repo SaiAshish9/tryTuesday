3e33caf40c953c8487f160973fa885562854e2ba
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2020-03-14 14:26:43 +0530

    Add files via upload

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -4,17 +4,42 @@ import {withRouter} from 'react-router-dom'
 
 const Feedback=()=>{
 
-  
+  const [submitted,isSubmitted]=useState(false)
 
 
 return(
     <div >
 
+{!isSubmitted?(
+<div>
 <h4 style={{padding:10,fontFamily:'Roberto',marginTop:'30vh'}}>
 Any other specifications you would like us to keep in mind while sending to you the recommendations.
 </h4>
 
+<textarea
+placeholder="Type your feedback here..."
+rows="10"
+style={{width:'100%',marginTop:10}}
+/>
 
+<button className="btn btn-outline-dark btn-lg" style={{position:'relative',left:'43.5%',top:10}}
+onClick={(e)=>{
+    e.preventDefault()
+    isSubmitted(true)
+}}
+
+>
+Submit
+</button>
+
+</div>
+):(
+<h4 style={{padding:10,fontFamily:'Roberto',marginTop:'30vh'}}>
+Your response is recorded.
+</h4>
+)}
+
+    
     </div>
 )
 
