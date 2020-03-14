2be7bda8aa2e082bb9fc9edbd612c90bcaaba697
SaiAshish9 <saiashish3760@gmail.com>
2020-03-14 14:35:59 +0530

    Updates

---
+++ precache-manifest.00b4851f50dce4ac12ea8cd1a4520459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c2ec782e7ca373868000529d5b334a",
+    "url": "/tryTuesday/index.html"
+  },
+  {
+    "revision": "2a35ed63aa005b4cef3c",
+    "url": "/tryTuesday/static/css/main.39b5d991.chunk.css"
+  },
+  {
+    "revision": "29a32e0220f700da45dd",
+    "url": "/tryTuesday/static/js/2.a62122f2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/tryTuesday/static/js/2.a62122f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a35ed63aa005b4cef3c",
+    "url": "/tryTuesday/static/js/main.b89af066.chunk.js"
+  },
+  {
+    "revision": "05178dfebd2d35dadc3c",
+    "url": "/tryTuesday/static/js/runtime-main.2b2e3fac.js"
+  }
+]);
